156546b355543591ea37dca455b313e6fd2f754a
Shauryanshi <shauryanshitripathi2005@gmail.com>
2026-04-23 10:54:29 +0530

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int f;
+    cout<<"enter the temp in f:";
+    cin>>f;
+
+    int c;
+    c=(f-32)*5/9;
+
+    cout<<"the temp in celsius is: "<<c;
+
+}
