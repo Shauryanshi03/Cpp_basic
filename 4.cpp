d98f6894d7004e78ae91c24967921bb4de02d15f
Shauryanshi <shauryanshitripathi2005@gmail.com>
2026-04-23 10:40:42 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b;
+    cout<<"enter the valu3e of a and b:  "<<endl;
+    cin>>a>>b;
+
+    if(a>b){
+        cout<<"a is greater"<<endl;
+
+    }
+    else{ 
+        cout<<"b is greater"<<endl;
+    }
+
+}
